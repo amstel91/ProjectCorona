f899dbd2e10968fb58f4a7cb504a95b24b351a27
Amstel <amsalmei@cisco.com>
2020-03-29 23:07:48 +0530

    Top Nav and Directory

--- CoronaApp/src/App.js
+++ CoronaApp/src/App.js
@@ -6,15 +6,23 @@
  * @flow
  */
 
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
+  Button,
   View,
   Text,
   StatusBar,
 } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import DashboardPage from './pages/dashboard/DashboardPage';
+import LocationComponent from './components/location/LocationComponent';
+import {createStackNavigator} from '@react-navigation/stack';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 
 import {
   Header,
@@ -23,11 +31,33 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-
+const Stack = createStackNavigator();
 const App: () => React$Node = () => {
+  let loadData = () =>{
+    
+  }
+  loadData();
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
+    <NavigationContainer>
+      <StatusBar barStyle = "dark-content" hidden = {false} backgroundColor = "#1081db" translucent = {true} />
+      <Stack.Navigator>
+        <Stack.Screen name="Dashboard" component={DashboardPage} options={{
+          headerTitle: props => <LocationComponent {...props} />,
+          headerStyle: {
+            backgroundColor: '#2196f3',
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+          headerRight: () => (
+            <View style={styles.global}>
+              <FontAwesomeIcon icon={ faGlobe } size={ 24 } color={ 'white' } />
+            </View>
+          )
+        }} />
+      </Stack.Navigator>
+      {/* <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
@@ -67,8 +97,8 @@ const App: () => React$Node = () => {
             <LearnMoreLinks />
           </View>
         </ScrollView>
-      </SafeAreaView>
-    </>
+      </SafeAreaView> */}
+    </NavigationContainer>
   );
 };
 
@@ -76,9 +106,9 @@ const styles = StyleSheet.create({
   scrollView: {
     backgroundColor: Colors.lighter,
   },
-  engine: {
+  global: {
     position: 'absolute',
-    right: 0,
+    right: 20,
   },
   body: {
     backgroundColor: Colors.white,
---
+++ CoronaApp/src/pages/dashboard/DashboardPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+
+class DashboardPage extends Component {
+    render() {
+        return (
+           <View>
+              <Text>
+                 Hello Dashboard
+              </Text>
+           </View>
+        )
+     }
+}
+
+export default DashboardPage;
