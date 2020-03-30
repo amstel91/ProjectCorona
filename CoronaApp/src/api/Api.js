65a6c78a766ed9565e31579a542648fc0150ef6d
Pratik Bisht <pratik92bisht@gmail.com>
2020-03-30 12:42:49 +0900

    Modified Api.js

--- CoronaApp/src/api/Api.js
+++ CoronaApp/src/api/Api.js
@@ -1,18 +1,24 @@
 class API{
     BASE_URL = "https://coronavirus-monitor.p.rapidapi.com/coronavirus";
 
-    /** 
-     * Dummy Method structure
-     * Usage in other file
-     * import API from {'./Api.js'}
-     * 
-     * API.getStatsByCountry("India").then(data => {
-     *  //do something
-     * })
-    */
-    static getStatsByCountry(countryName){
-        return fetch(BASE_URL+"/"+countryName);
+    static getWorldStats = () => {
+        const URL = BASE_URL + "/worldstat.php";
+        return fetch(URL)
+            .then((res) => res.json());
     }
+
+    static getCountryStats = (countryName) => {
+        const URL = BASE_URL + "/latest_stat_by_country.php?country=" + countryName;
+        return fetch(URL)
+            .then((res) => res.json());
+    }
+
+    static getCountryHistory = (countryName) => {
+        const URL = BASE_URL + "/cases_by_particular_country.php?country=" + countryName;
+        return fetch(URL)
+            .then((res) => res.json());
+}
+
 }
 
 export default API;
