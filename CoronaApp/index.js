48c47822a7b80c1b923faa92a7209e32801af96b
Pratik Bisht <pratik92bisht@gmail.com>
2020-04-01 02:45:31 +0900

    missed files

--- CoronaApp/index.js
+++ CoronaApp/index.js
@@ -5,5 +5,17 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import { Provider } from 'react-redux';
+import React from 'react';
 
-AppRegistry.registerComponent(appName, () => App);
+import configureStore from './src/store/configureStore'
+
+const store = configureStore()
+
+const RNRedux = () => (
+    <Provider store = { store }>
+      <App />
+    </Provider>
+  )
+
+AppRegistry.registerComponent(appName, () => RNRedux);
