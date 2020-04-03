801ee88f431261435e4b76b586a613d877a7ef58
Amstel <amsalmei@cisco.com>
2020-04-03 17:52:57 +0530

    Location page update

--- CoronaApp/src/pages/location/LocationPage.js
+++ CoronaApp/src/pages/location/LocationPage.js
@@ -1,5 +1,7 @@
 import { View, Text,StyleSheet,TextInput } from 'react-native';
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 class LocationSelectorComponent extends Component{
     constructor(props){
         super(props);
@@ -8,13 +10,23 @@ class LocationSelectorComponent extends Component{
     render(){
         return(
             <View>
-                <TextInput
-                    style={styles.searchBar}
-                    placeholder={"Search"}
-                    autoFocus={true}
-                    //onChangeText={text => onChangeText(text)}
-                    //value={value}
-                />
+                <View style={{flexDirection:"row",flex:1}}>
+                    <View style={{width:30,paddingTop:14,paddingLeft:10}}>
+                            <FontAwesomeIcon style={{justifyContent: 'flex-start'}} icon={ faSearch } size={ 20 } color={ '#c3c3c3' } />
+                    </View>
+                    <View>
+                        <TextInput
+                            style={styles.searchBar}
+                            placeholder={"Search"}
+                            autoFocus={true}
+                            textContentType={"countryName"}
+                            clearButtonMode={"always"}
+                            //onChangeText={text => onChangeText(text)}
+                            //value={value}
+                        />
+                    </View>
+                </View>
+                
             </View>
         )
     }
@@ -22,9 +34,9 @@ class LocationSelectorComponent extends Component{
 
 const styles = StyleSheet.create({
     searchBar:{
-        height: 40,
-        borderBottomColor: '#c3c3c3',
-        borderBottomWidth: 1,
+        height: 50,
+        justifyContent: 'flex-end',
+        fontSize:20
     }
 });
 
