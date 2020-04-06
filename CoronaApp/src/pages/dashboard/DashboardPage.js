a1805b90f4e5c04fb406ced277eff2bda8ab6289
Pratik Bisht <pratik92bisht@gmail.com>
2020-04-07 00:48:12 +0900

    Added scroll view to dashboard and pull-to-refresh

--- CoronaApp/src/pages/dashboard/DashboardPage.js
+++ CoronaApp/src/pages/dashboard/DashboardPage.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react'
-import { View, Text, StyleSheet, Colors,ActivityIndicator } from 'react-native'
+import React, { Component} from 'react'
+import { RefreshControl, ScrollView, View, Text, StyleSheet, Colors,ActivityIndicator } from 'react-native'
 import CardComponent from '../../components/card/CardComponent'
 import Api from '../../api/Api'
 import { connect } from 'react-redux';
 import {useSelector} from 'react-redux';
 import LocationComponent from '../../components/location/LocationComponent'
 import AppUtils from '../../utils/AppUtils'
+
+//const [refreshing, setRefreshing] = React.useState(false);
+
 class DashboardPage extends Component {
    state ={
+      refreshing: false,
       dashCountry:'',
       metadata: {total_cases:0, active_cases:0, total_recovered:0, total_deaths:0, record_date:''},
       line1 : {
@@ -44,26 +48,35 @@ class DashboardPage extends Component {
        }
    }
 
-componentWillReceiveProps(nextProp)
-{
-   //console.log(nextProp.country.countryName);
-   this.setState({
-      dashCountry:nextProp.country.countryName
-   });
-   metadata:Api.getCountryStats(nextProp.country.countryName)
+updateCountryStats(countryName){
+   return new Promise( (resolutionFunc,rejectionFunc) => {
+   metadata:Api.getCountryStats(countryName)
    .then((res) => {
       {
          //console.log(res);
          this.setState({
             metadata:res.latest_stat_by_country[0]
          });
-         this.populateExactData(nextProp.country.countryName);
+         this.populateExactData(countryName);
          //console.log(this.state.metadata.total_cases);
+         resolutionFunc();
       }
    })
    .catch(function(error) {
         console.log(error.message);
    });
+   
+}
+   )
+}
+
+componentWillReceiveProps(nextProp)
+{
+   //console.log(nextProp.country.countryName);
+   this.setState({
+      dashCountry:nextProp.country.countryName
+   });
+   this.updateCountryStats(nextProp.country.countryName);
 }
 
 populateExactData = (conutry) => {
@@ -149,6 +162,29 @@ static getDerivedStatFromProps(nextPros, prevState)
       }
       return "";
    }
+/*
+   wait(timeout) {
+      return new Promise(resolve => {
+        setTimeout(resolve, timeout);
+      });
+    }
+  
+    
+  
+   onRefresh = React.useCallback(() => {
+      setRefreshing(true);
+  console.log("refresh");
+      wait(2000).then(() => setRefreshing(false));
+    }, [refreshing]);
+*/
+
+
+_onRefresh() {
+   this.setState({refreshing: true});
+   this.updateCountryStats(this.state.dashCountry).then(() => {
+     this.setState({refreshing: false});
+   });
+}
 
     render() {
        if(this.state.line4.datasets[0].data === null){
@@ -164,6 +200,12 @@ static getDerivedStatFromProps(nextPros, prevState)
           //console.log('else con');
         return (
            <>
+    <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={this.state.refreshing}
+          onRefresh={this._onRefresh.bind(this)} />
+        }
+      >
                <View>
                   <View style={{backgroundColor: '#f9f9f9',paddingVertical:5,paddingHorizontal:10}}>
                      <Text style={styles.dashCountryText}>Statistics for {this.state.dashCountry}</Text>
@@ -177,6 +219,7 @@ static getDerivedStatFromProps(nextPros, prevState)
                   <CardComponent lineData={this.state.line3} count={this.state.metadata.total_recovered} header="RECOVERED" color="#4DB052"/>
                   <CardComponent lineData={this.state.line4} count={this.state.metadata.total_deaths} header="DECEASED" color="#616161"/>
                </View>
+               </ScrollView>
             </>
         )
        }
